8c6a267854e5a5e42a7025008953c5dead1d8970
nikola <nikola4work@gmail.com>
2019-11-18 16:32:19 -0500

    changed the alert message

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -25,7 +25,7 @@ function myFunction() {
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
+    alert("Password is Copied");
 }
 
 // for loop
