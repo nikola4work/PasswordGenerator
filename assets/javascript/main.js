aec86d306940ea10e83b814a4a18f3aac1123b81
nikola <nikola4work@gmail.com>
2019-11-18 15:25:18 -0500

    creted a function with for loop inside,to generate randoim char

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,8 @@
 // variables //
-
+var char = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
 var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var lower = "qwertyuiopasdfghjklzxcvbnm";
-var symbols = '!@#$%^&*()_+/*<>:"';
+var symbols = "!@#$%^&*()_+/*<>:";
 var numbers = "123456789";
 
 var charNum = document.getElementById("charNum");
@@ -13,4 +13,13 @@ var lowercase = document.getElementById("lower");
 var btnGen = document.getElementById("generate");
 var btnClip = document.getElementById("clipboard");
 
-console.log(symbols);
+console.log(password(12, char));
+
+// functions //
+function password(l, characters) {
+    var pwd = "";
+    for (var i = 0; i < l; i++) {
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
+}
