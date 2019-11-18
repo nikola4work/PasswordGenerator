7f8a9cfc348e47b9ec9c0cf3fe72cd49019ef3a6
nikola <nikola4work@gmail.com>
2019-11-18 15:45:59 -0500

    removed check box for upper,lowecase... password generates on its on those latters

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,21 @@
-// variables //
-var char = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
-var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
-var lower = "qwertyuiopasdfghjklzxcvbnm";
-var symbols = "!@#$%^&*()_+/*<>:";
-var numbers = "123456789";
+// variables
+var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*=-_";
 
 var charNum = document.getElementById("charNum");
 var numBox = document.getElementById("num");
 var symBox = document.getElementById("sym");
-var uppercase = document.getElementById("upper");
-var lowercase = document.getElementById("lower");
-var btnGen = document.getElementById("generate");
-var btnClip = document.getElementById("clipboard");
+var submit = document.getElementById("generate");
+var yourPw = document.getElementById("yourPw");
 
-console.log(password(12, char));
+submit.addEventListener("click", function(e) {
+    var characters = char;
+    numBox.checked ? (characters += num) : "";
+    symBox.checked ? (characters += sym) : "";
+    yourPw.value = password(charNum.value, characters);
+});
 
-// functions //
 function password(l, characters) {
     var pwd = "";
     for (var i = 0; i < l; i++) {
