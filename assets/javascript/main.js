b7246eb9408c8e0964ada48d1ad859df3f19fe6e
nikola <nikola4work@gmail.com>
2019-11-18 14:58:45 -0500

    corrected variable names and declaration

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,16 @@
-alert("hello");
+// variables //
+
+var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lower = "qwertyuiopasdfghjklzxcvbnm";
+var symbols = '!@#$%^&*()_+/*<>:"';
+var numbers = "123456789";
+
+var charNum = document.getElementById("charNum");
+var numBox = document.getElementById("num");
+var symBox = document.getElementById("sym");
+var uppercase = document.getElementById("upper");
+var lowercase = document.getElementById("lower");
+var btnGen = document.getElementById("generate");
+var btnClip = document.getElementById("clipboard");
+
+console.log(symbols);
