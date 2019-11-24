c71eacf69669f4d9d0390248e1f8561eaec057c2
nikola <nikola4work@gmail.com>
2019-11-24 15:33:12 -0500

    changes on instructions button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -780,11 +780,16 @@ if (text) {
 
 //DIV DROPDOWN
 
-function hiddenDiv() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+$(".Show").click(function() {
+    $("#target").show(500);
+    $(".Show").hide(0);
+    $(".Hide").show(0);
+});
+$(".Hide").click(function() {
+    $("#target").hide(500);
+    $(".Show").show(0);
+    $(".Hide").hide(0);
+});
+$(".toggle").click(function() {
+    $("#target").toggle("slow");
+});
